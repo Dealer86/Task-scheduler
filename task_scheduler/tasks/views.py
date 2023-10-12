37736a60e89e73c0ddf8b5927daef7a4e373d392
Dealer86 <adicroitor86@yahoo.com>
2023-10-12 16:34:22 +0300

    Added views for home and about

--- task_scheduler/tasks/views.py
+++ task_scheduler/tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    # return HttpResponse("<h1>Home</h1>")
+    return render(request, "tasks/home.html")
+
+
+def about(request):
+    # return HttpResponse("<h1>About</h1>")
+    return render(request, "tasks/about.html")
