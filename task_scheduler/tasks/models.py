d4c099bfc167f4b6c72c6c8473c8ebc18d9e5262
Dealer86 <adicroitor86@yahoo.com>
2023-10-21 11:47:53 +0300

    Added models for Tasks that also contains user authentication

--- task_scheduler/tasks/models.py
+++ task_scheduler/tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tasks(models.Model):
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    task_name = models.CharField(max_length=300)
+    date_created = models.DateField(auto_now_add=True)
+    date_deadline = models.DateTimeField()
+
+    def __str__(self):
+        return self.task_name
+
+
